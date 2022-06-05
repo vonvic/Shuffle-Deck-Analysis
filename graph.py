2c25197f2c59f1171d0e5a7a723fa62d0ee647a1
vonvic <40438077+vonvic@users.noreply.github.com>
2022-06-05 13:51:11 -0500

    deleted logfit function

--- graph.py
+++ graph.py
@@ -1,16 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def _logFit(x,y):
-    # cache some frequently reused terms
-    sumy = np.sum(y)
-    sumlogx = np.sum(np.log(x))
-
-    b = (x.size*np.sum(y*np.log(x)) - sumy*sumlogx)/(x.size*np.sum(np.log(x)**2) - sumlogx**2)
-    a = (sumy - b*sumlogx)/x.size
-
-    return a,b
-
 def plot(A: list, title: str = '', xlabel: str = '', ylabel: str = ''):
     x = np.array([(i+1) for i in range(len(A))])
     y = np.array(A)
