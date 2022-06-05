ce07023691dab5bbca2e8b397dde1896d86709f6
vonvic <40438077+vonvic@users.noreply.github.com>
2022-06-05 09:44:12 -0500

    init

---
+++ __main__.py
@@ -0,0 +1,48 @@
+from functools import reduce
+from math import sqrt
+from random import random
+
+def e_distance(A: list):
+    return reduce(lambda p, x: p+sqrt(pow(x-A.index(x), 2)), A)
+
+def shuffle(A: list):
+    mid = len(A)//2
+    fst, snd = A[:mid], A[mid:]
+    
+    new_list = []
+    for i in range(len(A)):
+        value = random()
+        
+        chosen: int
+        if value < 0.5:
+            chosen = fst.pop(0) if fst else snd.pop(0)
+        else:
+            chosen = snd.pop(0) if snd else fst.pop(0)
+        new_list.append(chosen)
+        
+    return new_list
+
+def print_sorted_measurement(A: list):
+    sorted_measure = e_distance(A)
+    print(sorted_measure)
+
+def main():
+    sorted_deck = [i for i in range(52)]
+    deck = sorted_deck[:]
+    
+    shuffle_count = 100
+    
+    num_of_repeats = 1000
+    averages = [0 for _ in range(shuffle_count)]
+    for _ in range(num_of_repeats):
+        for i in range(shuffle_count):
+            deck = shuffle(deck)
+            sorted_measure = e_distance(deck)
+            averages[i] += sorted_measure
+        deck = sorted_deck[:]
+    
+    averages = list(map(lambda x: x/num_of_repeats, averages))
+    for x in averages: print(x)
+
+if __name__ == '__main__':
+    main()
